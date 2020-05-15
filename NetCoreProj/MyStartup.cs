abff7f56205df9e4603b036524e06e004ab0b4e6
Reznychenko Sergey <sergeyrez.sr@gmail.com>
2020-05-15 09:26:36 +0300

    1

---
+++ NetCoreProj/MyStartup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace NetCoreProj
+{
+    public class MyStartup
+    {
+        public IConfiguration Configuration { get; }
+
+        public MyStartup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync($"{Configuration["Logging:LogLevel:Microsoft.Hosting.Lifetime"]}");
+                });
+            });
+        }
+    }
+}
